93f5722e635cbcd5819cd8bebf59f70fc8317f1f
Jesse Burdick-Pless <jb4411@rit.edu>
2020-07-11 17:18:01 -0400

    Added documentation and comments to ConnectFourGUI

--- src/connectfour/gui/ConnectFourGUI.java
+++ src/connectfour/gui/ConnectFourGUI.java
@@ -33,23 +33,37 @@ public class ConnectFourGUI extends Application implements Observer<ConnectFourB
     /** player 2 space image */
     private Image p2 = new Image(getClass().getResourceAsStream("p2red.png"));
 
+    /** the HBox that stores the labels for the status bar */
     private HBox statusBar;
+    /** the label that stores the number of move made for the status bar */
     private Label movesMade;
+    /** the label that stores the current player for the status bar */
     private Label currentPlayer;
+    /** the label that stores the current game status for the status bar */
     private Label gameStatus;
+    /** the game board */
     private ConnectFourBoard board;
-
+    /** a 2D array of every button in the board */
     private ConnectButton[][] buttonBoard;
-
+    /** the column that a piece was last placed in */
     private int lastPlaced;
+    /** whether or not the game has ended */
     private boolean finished;
 
-
     private class ConnectButton extends Button {
+        /** this button's row */
         private int row;
+        /** this button's column */
         private int col;
+        /** this button's owner */
         private ConnectFourBoard.Player owner;
 
+        /**
+         * A helper function that changes the owner and image of the button
+         * passed in through the parameter.
+         *
+         * @param owner the button to have it's image and owner changed
+         */
         public void changeOwner(ConnectFourBoard.Player owner) {
             this.owner = owner;
             switch (owner) {
@@ -60,6 +74,13 @@ public class ConnectFourGUI extends Application implements Observer<ConnectFourB
 
         }
 
+        /**
+         * Create a new ConnectButton with no owner and an image of an empty
+         * space.
+         *
+         * @param row this button's row
+         * @param col this button's column
+         */
         public ConnectButton(int row, int col) {
             this.row = row;
             this.col = col;
@@ -68,6 +89,12 @@ public class ConnectFourGUI extends Application implements Observer<ConnectFourB
         }
     }
 
+    /**
+     * A helper function that builds a grid of buttons used as the GUI
+     * representation of the board.
+     *
+     * @return the grid pane representing the board
+     */
     private GridPane makeBoard() {
         GridPane gridPane = new GridPane();
 
@@ -94,29 +121,32 @@ public class ConnectFourGUI extends Application implements Observer<ConnectFourB
      * @param stage container (window) in which to render the GUI
      * @throws Exception if there is a problem
      */
+    @Override
     public void start( Stage stage ) throws Exception {
         //create the border pane that holds the board and status info
         BorderPane borderPane = new BorderPane();
+        //create the board
         this.board = new ConnectFourBoard();
         this.board.addObserver(this);
+
+        //initialize variables
         this.lastPlaced = 0;
         this.finished = false;
 
+        //create the 2D array of buttons
         this.buttonBoard = new ConnectButton[ConnectFourBoard.ROWS][ConnectFourBoard.COLS];
 
+        //create the status bar
         this.statusBar = new HBox();
         this.movesMade = new Label(this.board.getMovesMade() + " moves made ");
         this.currentPlayer = new Label("Current player: " + this.board.getCurrentPlayer());
         this.gameStatus = new Label("Status: " + this.board.getGameStatus());
-
         this.statusBar.getChildren().add(this.movesMade);
         this.statusBar.getChildren().add(this.currentPlayer);
         this.statusBar.getChildren().add(this.gameStatus);
         this.statusBar.setAlignment(Pos.CENTER);
         this.statusBar.setSpacing(100);
-
         borderPane.setBottom(this.statusBar);
-        //BorderPane.setAlignment(this.statusBar, Pos.CENTER);
 
         // get the grid pane from the helper method
         GridPane gridPane = makeBoard();
@@ -127,7 +157,6 @@ public class ConnectFourGUI extends Application implements Observer<ConnectFourB
         stage.setTitle("Connect Four GUI");
         stage.setScene(scene);
         stage.setResizable(false);
-
         stage.show();
     }
 
@@ -139,16 +168,20 @@ public class ConnectFourGUI extends Application implements Observer<ConnectFourB
      */
     @Override
     public void update(ConnectFourBoard connectFourBoard) {
+        //if the game has not ended update the view
         if (!this.finished) {
+            //update board
             ConnectFourBoard.Player current;
             for (int row = 0; row < ConnectFourBoard.ROWS; row++) {
                 current = this.board.getContents(row, this.lastPlaced);
                 this.buttonBoard[row][this.lastPlaced].changeOwner(current);
             }
-
+            //update status bar
             this.movesMade.setText(this.board.getMovesMade() + " moves made ");
             this.currentPlayer.setText("Current player: " + this.board.getCurrentPlayer());
             this.gameStatus.setText("Status: " + this.board.getGameStatus());
+
+            //if the game has ended set this.finished to true
             if (this.board.getGameStatus() != ConnectFourBoard.Status.NOT_OVER) {
                 this.finished = true;
             }
